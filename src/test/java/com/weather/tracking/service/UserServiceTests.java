5fc9f5cae7e9c31ecadeb1fade1337a5c4b3eaa1
Michael Jankowski <michael.jankowski@archtis.com>
2024-02-25 20:09:27 +1100

    Finish tests for UserService

--- src/test/java/com/weather/tracking/service/UserServiceTests.java
+++ src/test/java/com/weather/tracking/service/UserServiceTests.java
@@ -1,6 +1,7 @@
 package com.weather.tracking.service;
 
 import com.weather.tracking.dto.request.UserRegistrationRequestDto;
+import com.weather.tracking.exception.UserAlreadyExistsException;
 import com.weather.tracking.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,8 +12,11 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
@@ -36,5 +40,21 @@ public class UserServiceTests {
         assertDoesNotThrow(() -> service.registerUser(registrationRequest));
 
         verify(userRepository).save(argThat(user -> Objects.equals(user.getEmail(), registrationRequest.getEmail())));
+    }
+
+    @Test
+    void testUserCannotRegisterWithExistingEmail() {
+        final String email = "michael@email.com";
+        final String name = "mike";
+
+        UserRegistrationRequestDto registrationRequest = new UserRegistrationRequestDto();
+        registrationRequest.setEmail(email);
+        registrationRequest.setName(name);
+
+        doReturn(true).when(userRepository).existsByEmail(registrationRequest.getEmail());
+
+        assertThrows(UserAlreadyExistsException.class, () -> service.registerUser(registrationRequest));
+
+        verify(userRepository, never()).save(any());
     }
 }
